c250dc6cf5c04f6452987c659bda32f11ce3a30d
espressohn <espressohn@gmail.com>
2017-09-25 09:25:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,20 @@ var recipes = {
   Bake: "bread",
   Soup: "chicken noodle"
 }
+
+function updateObjectWithKeyAndValue(object, key, value){
+  object[key]=value
+  return object
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  
+}
+
+function deleteFromObjectByKey(object, key){
+  
+}
+
+function destructivelyDeleteFromObjectByKey(object, key){
+  
+}
