d1f2822af49ee6c91c0a80decea2be94419a4893
espressohn <espressohn@gmail.com>
2017-09-25 09:25:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,13 +10,13 @@ function updateObjectWithKeyAndValue(object, key, value){
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
-  
+
 }
 
 function deleteFromObjectByKey(object, key){
-  
+
 }
 
 function destructivelyDeleteFromObjectByKey(object, key){
-  
+
 }
